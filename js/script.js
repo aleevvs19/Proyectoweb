d0db400a541f305d81e8f736f1fd5d9b0ab09e93
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-08-24 16:18:19 -0400

    new cambioss

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Requisito 6: Función JavaScript para interactuar con la página
+function mostrarMensaje() {
+    alert("¡Bienvenido a MotorStore! Revisa nuestros catálogos de autos y motos.");
+}
+
+// Desafío opcional: Cambiar entre modo claro y modo oscuro
+function alternarModo() {
+    document.body.classList.toggle("modo-oscuro");
+}
